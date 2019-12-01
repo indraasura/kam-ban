45c3deb83cebcbd42092a59bbcf8dd41646e0b09
Swarup Hegde <swaruphegde@gmail.com>
2019-12-01 09:30:22 +0530

    feat: added static layout and css

--- kam-ban-app/src/App.jsx
+++ kam-ban-app/src/App.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import KanbanBoard from './components/KanbanBoard'
+import './App.css'
 
-const App = () => 
-<>
-    <h1>Hello World</h1>
-</>
+let cardsList = [
+    {
+        id: 1,
+        title: "Read the book",
+        description: " I should read the whole book",
+        status: "in-progress",
+        tasks: []
+    },
+    {
+        id: 2,
+        title: "Write some code",
+        description: " Code along the tutorial ",
+        status: "to-do",
+        tasks: [
+            {
+                id: 1,
+                name: "ContactList Example",
+                done: true
+            },
+            {
+                id: 2,
+                name: "Kanban example",
+                done: false
+            },
+            {
+                id: 3,
+                name: "My own experiments",
+                done: false
+            }
+        ]
+    },
+]
+
+const App = () => <KanbanBoard cards={cardsList} />
 
 export default App
